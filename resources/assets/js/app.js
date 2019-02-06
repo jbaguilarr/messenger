50395a8bf449edfc0b2952c2b811325ea100bd52
bruno aguilar <josebrunoaguilar@gmail.com>
2019-02-05 18:04:43 -0700

    Login UI avanzado

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -9,12 +9,10 @@ require('./bootstrap');
 
 window.Vue = require('vue');
 
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
+//import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 
+Vue.use(BootstrapVue);
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
 
 const app = new Vue({
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,52 @@
+@extends('layouts.app')
+
+@section('content')
+<b-container>
+    <b-row align-h="center">
+        <b-col cols="8">
+            <b-card title="Inicio de sesion">
+                <b-alert show>
+                    Por favor ingresa tus datos:
+                </b-alert>
+                <form method="POST" action="{{ route('login') }}">
+                        {{ csrf_field() }}
+                         <b-form-group
+                                        label="Correo electronico:"
+                                        label-for="email"
+                                        description="No compartiremos su correo">
+                            <b-form-input id="email"
+                                          type="email"
+                                          name="email"
+                                          value="{{ old('email') }}" 
+                                          required autofocus                                          
+                                          placeholder="josebrunoaguilar@gmail.com">
+                            </b-form-input>
+                          </b-form-group>
+                            <b-form-group label="Contrasena:" label-for="password">
+                            <b-form-input id="password"
+                                          type="password"
+                                          name="password"
+                                          value="{{ old('password') }}" 
+                                          required>
+                            </b-form-input>
+                          </b-form-group>  
+                        <b-form-group>                  
+                            <b-form-checkbox name="remember" {{ old('remember') ? 'checked="true"' : '' }}>
+                                Recordar session
+                            </b-form-checkbox> 
+                        </b-form-group>
+                      
+                         <b-button type="submit" variant="primary">
+                            Ingresar
+                         </b-button>
+                         <b-button href="{{ route('password.request') }}" variant="link">
+                             Olvidaste tu Contrasena
+                         </b-button>   
+
+                    </form>
+            </b-card>
+        
+        </b-col>
+    </b-row>
+</b-container>
+@endsection
